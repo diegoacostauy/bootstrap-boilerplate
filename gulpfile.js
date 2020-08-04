86b50b978cf430d15ab01caad2a8a2260c2ae87b
diegaco <diegoacosta211@gmail.com>
2020-08-03 23:43:47 -0300

    add partials support, dev and build tasks, improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const gulp = require('gulp'),
   sass = require('gulp-sass'),
@@ -9,18 +9,21 @@ const gulp = require('gulp'),
   buffer = require('vinyl-buffer'),
   uglify = require('gulp-uglify'),
   cleanCSS = require('gulp-clean-css'),
-  rename = require("gulp-rename"),
+  rename = require('gulp-rename'),
   merge = require('merge-stream'),
   htmlreplace = require('gulp-html-replace'),
+  fileinclude = require('gulp-file-include'),
+  htmlmin = require('gulp-htmlmin'),
   autoprefixer = require('gulp-autoprefixer'),
   sourcemaps = require('gulp-sourcemaps'),
   browserSync = require('browser-sync').create();
 
 // Clean task
 gulp.task('clean', function(cb) {
-  del(['dist', 'assets/css/app.css']);
+  del(['dev', 'dist', 'assets/css/app.css']);
   cb();
 });
+
 
 // Copy third party libraries from node_modules into /vendor
 gulp.task('vendor:js', function() {
@@ -48,18 +51,6 @@ gulp.task('vendor:fonts', function() {
 // vendor task
 gulp.task('vendor', gulp.parallel('vendor:fonts', 'vendor:js'));
 
-// Copy vendor's js to /dist
-gulp.task('vendor:build', function() {
-  var jsStream = gulp.src([
-    './assets/js/vendor/bootstrap.bundle.min.js',
-    './assets/js/vendor/jquery.slim.min.js',
-    './assets/js/vendor/popper.min.js'
-  ])
-    .pipe(gulp.dest('./dist/assets/js/vendor'));
-  var fontStream = gulp.src(['./assets/fonts/font-awesome/**/*.*']).pipe(gulp.dest('./dist/assets/fonts/font-awesome'));
-  return merge (jsStream, fontStream);
-})
-
 // Copy Bootstrap SCSS(SASS) from node_modules to /assets/scss/bootstrap
 gulp.task('bootstrap:scss', function() {
   return gulp.src(['./node_modules/bootstrap/scss/**/*'])
@@ -84,13 +75,75 @@ gulp.task('scss', gulp.series('bootstrap:scss', function compileScss() {
       outputStyle: 'expanded'
     }).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write('./dev/assets/css/maps'))
     .pipe(gulp.dest('./assets/css'))
 }));
 
+
+// DEV TASKS
+// Copy vendor's js to /dev
+gulp.task('vendor:dev', function() {
+  var jsStream = gulp.src([
+    './assets/js/vendor/bootstrap.min.js',
+    './assets/js/vendor/jquery.slim.min.js',
+    './assets/js/vendor/popper.min.js'
+  ])
+    .pipe(gulp.dest('./dev/assets/js/vendor'));
+  var fontStream = gulp.src(['./assets/fonts/font-awesome/**/*.*']).pipe(gulp.dest('./dev/assets/fonts/font-awesome'));
+  return merge (jsStream, fontStream);
+})
+
+// Css dev
+gulp.task('css:dev', gulp.series('scss', function cssMinify() {
+  return gulp.src('./assets/css/app.css')
+    .pipe(gulp.dest('./dev/assets/css'))
+    .pipe(browserSync.stream());
+}));
+
+// Js dev
+gulp.task('js:dev', function () {
+  return browserify({
+    entries: ['./assets/js/index.js']
+  })
+  .transform(babelify,{presets: ['@babel/preset-env']})
+  .bundle()
+  .pipe(source('index.js'))
+  .pipe(buffer())
+  .pipe(rename('app.min.js'))
+  .pipe(sourcemaps.init({ loadMaps: true }))
+  .pipe(sourcemaps.write('./dev/assets/js/maps'))
+  .pipe(gulp.dest('./dev/assets/js'))
+  .pipe(browserSync.stream())
+});
+
+gulp.task('html:dev', () => {
+  return gulp.src('*.html')
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file',
+      indent: true
+    }))
+    .pipe(gulp.dest('./dev'));
+});
+
+gulp.task('fileinclude:dev', function(callback) {
+  return gulp
+    .src('*.html')
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file',
+      indent: true
+    }))
+    .pipe(gulp.dest('./dev'));
+});
+// DEV TASKS
+
+
+
+// BUILD TASKS
 // Minify CSS
-gulp.task('css:minify', gulp.series('scss', function cssMinify() {
-  return gulp.src("./assets/css/app.css")
+gulp.task('css:build', gulp.series('scss', function cssMinify() {
+  return gulp.src('./assets/css/app.css')
     .pipe(cleanCSS())
     .pipe(rename({
       suffix: '.min'
@@ -100,16 +153,7 @@ gulp.task('css:minify', gulp.series('scss', function cssMinify() {
 }));
 
 // Minify Js
-gulp.task('js:minify', function () {
-  // return gulp.src([
-  //   './assets/js/index.js'
-  // ])
-  //   .pipe(uglify())
-  //   .pipe(rename({
-  //     suffix: '.min'
-  //   }))
-  //   .pipe(gulp.dest('./dist/assets/js'))
-  //   .pipe(browserSync.stream());
+gulp.task('js:build', function () {
   return browserify({
     entries: ['./assets/js/index.js']
   })
@@ -125,44 +169,76 @@ gulp.task('js:minify', function () {
   .pipe(browserSync.stream())
 });
 
-// Replace HTML block for Js and Css file upon build and copy to /dist
-gulp.task('replaceHtmlBlock', function () {
-  return gulp.src(['*.html'])
+// Copy vendor's js to /dist
+gulp.task('vendor:build', function() {
+  var jsStream = gulp.src([
+    './assets/js/vendor/bootstrap.min.js',
+    './assets/js/vendor/jquery.slim.min.js',
+    './assets/js/vendor/popper.min.js'
+  ])
+    .pipe(gulp.dest('./dist/assets/js/vendor'));
+  var fontStream = gulp.src(['./assets/fonts/font-awesome/**/*.*']).pipe(gulp.dest('./dist/assets/fonts/font-awesome'));
+  return merge (jsStream, fontStream);
+})
+
+gulp.task('html:build', () => {
+  return gulp.src('*.html')
     .pipe(htmlreplace({
       'js': 'assets/js/app.min.js',
       'css': 'assets/css/app.min.css'
     }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file',
+      indent: true
+    }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+// BUILD TASKS
+
+gulp.task('browsersync', function(callback) {
+  browserSync.init({
+    server: {
+      baseDir: ['./dev', './']
+    },
+  });
+  callback();
+});
+
+gulp.task('browsersyncReload', function(callback) {
+  browserSync.reload();
+  callback();
 });
 
 // Configure the browserSync task and watch file path for change
-gulp.task('dev', function browserDev(done) {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  gulp.watch(['./assets/scss/**/*.scss','!assets/scss/bootstrap/**'], gulp.series('css:minify', function cssBrowserReload (done) {
-    browserSync.reload();
-    done(); //Async callback for completion.
-  }));
-  gulp.watch('./assets/js/**/.js', gulp.series('js:minify', function jsBrowserReload (done) {
-    browserSync.reload();
-    done();
-  }));
-  gulp.watch(['*.html']).on('change', browserSync.reload);
-  done();
+gulp.task('watch', function browserDev(done) {
+  gulp.watch(['./assets/scss/**/*.scss','!assets/scss/bootstrap/**'], gulp.series('css:dev','browsersyncReload'));
+  gulp.watch('./assets/js/**/.js', gulp.series('js:dev','browsersyncReload'));
+  gulp.watch(['*.html', 'partials/*.html'], gulp.series('html:dev', 'browsersyncReload'));
 });
 
-// Build task
-gulp.task("build", gulp.series(gulp.parallel('css:minify', 'js:minify', 'vendor'), 'vendor:build', function copyAssets() {
+// Dev task live reload
+gulp.task('dev:build', gulp.series(gulp.parallel('css:dev', 'js:dev', 'vendor'), 'vendor:dev', function copyAssetsDev() {
   return gulp.src([
     '*.html',
     // 'favicon.ico',
-    "assets/img/**"
+    'assets/img/**'
+  ], { base: './'})
+    .pipe(gulp.dest('dev'));
+}));
+
+// Build task
+gulp.task('prod:build', gulp.series(gulp.parallel('css:build', 'js:build', 'vendor'), 'vendor:build', function copyAssets() {
+  return gulp.src([
+    '*.html',
+    // 'favicon.ico',
+    'assets/img/**'
   ], { base: './'})
     .pipe(gulp.dest('dist'));
 }));
 
 // Default task
-gulp.task("default", gulp.series("clean", 'build', 'replaceHtmlBlock'));
+gulp.task('default', gulp.series('clean', 'prod:build', 'html:build'));
+gulp.task('dev', gulp.series('clean', 'dev:build', 'html:dev', gulp.parallel('browsersync', 'watch')));
